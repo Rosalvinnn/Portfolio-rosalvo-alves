5f993a953c90af9ae2b2623c7a05f3f86cff9bf4
Rosalvinnn <rosalvo.00000854909@unicap.br>
2026-09-22 22:37:02 +0000

    correção

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -317,6 +317,20 @@ export default function Home() {
               </div>
               <span className="project-art-label">DATA & STRUCTURE</span>
             </div>
+
+            <div className="project-info">
+              <div className="project-type">BANCO DE DADOS</div>
+              <h3>Sistema de Clínica</h3>
+              <p>
+                Modelagem de um banco de dados para organizar
+                pacientes, médicos, consultas e receitas.
+              </p>
+              </div>
+              <div className="tag-list">
+                <span>MySQL</span>
+                <span>SQL</span>
+                <span>Modelagem</span>
+              </div>
           </article>
 
           <article className="project-card">
